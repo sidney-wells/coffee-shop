941a2e02af3b04178cb82896055fced7315f602d
Sidney Wells <sidneywells101@gmail.com>
2020-08-03 08:23:51 -0500

    feedback changes

--- backend/src/api.py
+++ backend/src/api.py
@@ -9,31 +9,37 @@ from .auth.auth import AuthError, requires_auth
 
 app = Flask(__name__)
 setup_db(app)
-CORS(app,resources={r"*": {"origins": "*"}})
+CORS(app, resources={r"*": {"origins": "*"}})
+
 
 @app.after_request
 def after_request(response):
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    response.headers.add('Access-Control-Allow-Headers',
+                         'Content-Type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods',
+                         'GET,PUT,POST,DELETE,OPTIONS')
     response.headers.add('Access-Control-Allow-Credentials', 'true')
     return response
 
+
 db_drop_and_create_all()
+
 
 @app.route('/drinks', methods=['GET'])
 def get_drinks(*args, **kwargs):
     
     drinks = Drink.query.order_by(Drink.id).all()
     
-    drinks = [drink.short() for drink in drinks]  
+    drinks = [drink.short() for drink in drinks]
     
     if len(drinks) == 0:
-        drinks = [] 
-    
+        drinks = []
+
     return jsonify({
-        'success': True, 
+        'success': True,
         'drinks': drinks
     })
+
 
 @app.route('/drinks-detail', methods=['GET'])
 @requires_auth('get:drinks-detail')
@@ -41,13 +47,13 @@ def get_drinks_detail(*args, **kwargs):
     
     drinks = Drink.query.order_by(Drink.id).all()
         
-    drinks = [drink.long() for drink in drinks]    
+    drinks = [drink.long() for drink in drinks]
         
     if len(drinks) == 0:
         drinks = []
     
     return jsonify({
-        'success': True, 
+        'success': True,
         'drinks': drinks
     })
 
@@ -69,10 +75,10 @@ def create_drink(*args, **kwargs):
         drink.insert()
         
         return jsonify({
-        'success': True,
-        'drinks': drink.long()
-    })
-    except: 
+            'success': True,
+            'drinks': drink.long()
+        })
+    except():
         abort(404)
     
     
@@ -84,7 +90,7 @@ def update_drink(*args, **kwargs):
     
     body = request.get_json()
     
-    if body is None: 
+    if body is None:
         abort(404)
         
     title = body.get('title', None)
@@ -95,34 +101,35 @@ def update_drink(*args, **kwargs):
         drink.title = title
         
         drink.recipe = json.dumps([recipe])
-            
+         
         drink.update()
-        
+      
         return jsonify({
-        'success': True,
-        'drinks': [drink.long()]
+            'success': True,
+            'drinks': [drink.long()]
         })
-    except:
+    except():
         abort(404)
 
 
 @app.route('/drinks/<int:drink_id>', methods=['DELETE'])
 @requires_auth('delete:drinks')
 def delete_drink(*args, **kwargs):
-    
+
     drink_id = kwargs['drink_id']
-    
+
     drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
-    
+
     if drink is None:
         abort(404)
-    
+
     drink.delete()
-    
+
     return jsonify({
-    'success': True,
-    'delete': drink_id
+        'success': True,
+        'delete': drink_id
     })
+
 
 @app.errorhandler(422)
 def unprocessable(error):
@@ -131,8 +138,8 @@ def unprocessable(error):
         'error': 422,
         'message': "unprocessable"
     }), 422
-  
-  
+
+
 @app.errorhandler(401)
 def unauthorized(error):
     return jsonify({
@@ -140,7 +147,8 @@ def unauthorized(error):
         'error': 401,
         'message': 'authorization failed'
     })
-      
+
+
 @app.errorhandler(400)
 def bad_request(error):
     return jsonify({
@@ -148,7 +156,8 @@ def bad_request(error):
         'error': 400,
         'message': "bad request"
     }), 400
-    
+
+
 @app.errorhandler(405)
 def not_found(error):
     return jsonify({
@@ -156,6 +165,7 @@ def not_found(error):
         'error': 405,
         'message': "method not allowed"
     }), 405
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -173,6 +183,7 @@ def forbidden(error):
         'error': 403,
         'message': 'forbidden'
     })
+
 
 @app.errorhandler(AuthError)
 def auth_failed(AuthError):
